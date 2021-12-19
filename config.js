03079578facfb7839fb5409f7e440e9dacd3132b
gabrieledarrigo <gabriele.darrigo@studenti.unimi.it>
2021-12-19 20:01:00 +0100

    Move configurations constants to config module

---
+++ config.js
@@ -0,0 +1,17 @@
+const NUMBER_OF_ROWS = process.env.NUMBER_OF_ROWS || 5_000_000;
+const MIN_LATITUDE = process.env.MIN_LATITUDE || 45.38676;
+const MAX_LATITUDE = process.env.MAX_LATITUDE || 45.53582;
+const MIN_LONGITUDE = process.env.MIN_LONGITUDE || 9.04091;
+const MAX_LONGITUDE = process.env.MAX_LONGITUDE || 9.27808;
+const MIN_ALTITUDE = process.env.MIN_ALTITUDE || 1;
+const MAX_ALTITUDE = process.env.MAX_ALTITUDE || 10;
+
+module.exports = {
+  NUMBER_OF_ROWS,
+  MIN_LATITUDE,
+  MAX_LATITUDE,
+  MIN_LONGITUDE,
+  MAX_LONGITUDE,
+  MIN_ALTITUDE,
+  MAX_ALTITUDE,
+};
--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const fs = require('fs');
 const path = require('path');
 // const jsonexport = require('jsonexport');
 const {
+  NUMBER_OF_ROWS, MIN_LATITUDE, MAX_LATITUDE, MIN_LONGITUDE, MAX_LONGITUDE, MIN_ALTITUDE, MAX_ALTITUDE,
+} = require('./config');
+const {
   getId, getSensor, getLocation, getMeasurement, getMeasurementTimestamps, getAgent,
-} = require('./random');
+} = require('./lib/random');
 
-const NUMBER_OF_ROWS = 5_000_000;
-const JSON_OUTPUT = path.resolve(`${__dirname}/data/output`, `dl_measurements-${Date.now()}.ndjson`);
-const MIN_LATITUDE = 45.38676;
-const MAX_LATITUDE = 45.53582;
-const MIN_LONGITUDE = 9.04091;
-const MAX_LONGITUDE = 9.27808;
-const MIN_ALTITUDE = 1;
-const MAX_ALTITUDE = 10;
+const JSON_OUTPUT = path.resolve(`${__dirname}/output`, `dl_measurements-${Date.now()}.ndjson`);
 
 (function run() {
   const stream = fs.createWriteStream(JSON_OUTPUT, { flags: 'a' });
